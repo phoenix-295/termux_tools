6201ccba6c153ca93006d9f023ac4e3749a1bb76
Nikhil Shirdhankar <nikhil295@gmail.com>
2023-02-03 10:15:52 +0530

    bash script changes

--- simple_mmo_boss/bash_nxt_boss.sh
+++ simple_mmo_boss/bash_nxt_boss.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-python3 /data/data/com.termux/files/home/.shortcuts/termux_tools/simplemmo_boss_timer/next_boss_timer.py
+python3 /data/data/com.termux/files/home/.shortcuts/termux_tools/simple_mmo_boss/next_boss_timer.py
 read junk
