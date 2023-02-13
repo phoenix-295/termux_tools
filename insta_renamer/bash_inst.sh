3ca37110528d9b83f13345ee490ac470d7072284
Nikhil Shirdhankar <nikhil295@gmail.com>
2023-02-13 10:30:15 +0530

    insta: file rename

--- insta_renamer/bash_inst.sh
+++ insta_renamer/bash_inst.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cd /data/data/com.termux/files/home/storage/shared/Pictures/Instander
+cd /data/data/com.termux/files/home/storage/shared/Python/termux_tools/insta_renamer
 python3 insta_mapper.py
 read junk
