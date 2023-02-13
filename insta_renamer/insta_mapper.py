39939e7697f9bf3a5d0a6696da71e91a6bb8e080
Nikhil Shirdhankar <nikhil295@gmail.com>
2023-02-13 10:04:52 +0530

    insta_mapper

---
+++ insta_renamer/insta_mapper.py
@@ -0,0 +1,28 @@
+import os
+
+downloadsFolder = "C:\\Users\\Nikhil\\Downloads"
+
+
+def find_webp_files(path):
+    zip_files = []
+    files = os.listdir(path)
+    for file in files:
+        if file.endswith(".webp"):
+            zip_files.append(file)
+    return zip_files
+
+def rename_webp(path):
+    for each in path:
+        base, ext = os.path.splitext(each)
+        new_file = base + ".jpeg"
+        # print(f"{downloadsFolder}\{each}")
+        # print(f"{downloadsFolder}\{new_file}")
+        os.rename(f"{downloadsFolder}\{each}", f"{downloadsFolder}\{new_file}")
+
+def main():
+    doc_files = find_webp_files(downloadsFolder)
+    print(doc_files)
+    rename_webp(doc_files)
+
+if __name__ == "__main__":
+    main()
